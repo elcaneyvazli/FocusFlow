7c71a956402ac28411d6bb002bfd0c6e2414289d
“elcaneyvazli” <elcaneyvazli77@gmail.com>
2024-06-09 13:44:14 +0400

    find vercel proble and solve

--- src/app/dashboard/timeanalysis/page.jsx
+++ src/app/dashboard/timeanalysis/page.jsx
@@ -1,10 +1,8 @@
-import { CustomKanban } from '@/ui/component/Dashboard/todotask/kanbancard/test'
 import React from 'react'
 
 export default function page() {
   return (
     <div>
-      <CustomKanban />
     </div>
   )
 }
