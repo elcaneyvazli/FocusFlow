b18251fd2decbfbf7d898f603ea8ca61d10e7cd0
“elcaneyvazli” <elcaneyvazli77@gmail.com>
2025-01-13 22:27:10 +0400

    update:latest changes

--- src/ui/module/components/DashboardPage/TodotaskPage/TodotaskPage.jsx
+++ src/ui/module/components/DashboardPage/TodotaskPage/TodotaskPage.jsx
@@ -1,6 +1,5 @@
 "use client";
 import Button from "@/ui/module/blocks/Button/Button";
-import Tab from "@/ui/module/blocks/Tab/Tab";
 import { LayoutGrid as GridIcon, List as ListIcon } from "lucide-react";
 import useScreenWidth from "@/ui/module/utils/UseScreenWidth/useScreenWidth";
 import AnalysisCardContainer from "./AnalysisCard/AnalysisCardContainer";
@@ -8,6 +7,19 @@ import dynamic from "next/dynamic";
 import Spinner from "@/ui/module/blocks/Spinner/Spinner";
 const BoardContainer = dynamic(() => import("./Board/BoardContainer"), {
   loading: () => <Spinner />,
+});
+const Tab = dynamic(() => import("@/ui/module/blocks/Tab/Tab"), {
+  loading: () => (
+    <div className="relative flex flex-col sm:flex-row items-center justify-between sm:gap-0 gap-4 border-0 sm:border-b border-border">
+      <div className="flex flex-row items-center justify-center gap-8 px-16 py-12 w-full sm:w-fit relative border-0 text-light">
+        <div className="h-[36px] w-64 bg-gray-300 dark:bg-gray-700 rounded-md" />
+        <div className="h-[36px] w-64 bg-gray-300 dark:bg-gray-700 rounded-md" />
+      </div>
+      <div className="flex flex-row gap-16 items-end justify-end w-full">
+        <div className="h-[36px] w-full sm:w-64 bg-gray-300 dark:bg-gray-700 rounded-md" />
+      </div>
+    </div>
+  ),
 });
 
 export default function TodotaskPage() {
